e548046b95f0339b3279f363775f1253cc55ad52
vraj1412 <sq.vraj1410@gmail.com>
2024-09-13 12:08:23 +0530

    BANK_PRO.cpp

--- CPP/BANK_PRO.cpp
+++ CPP/BANK_PRO.cpp
@@ -83,22 +83,22 @@ int main()
 {
     Bank user[15];
 
-    user[0] = Bank{"Vraj Kachhadiya", 300556477845, "SBI60459CO", 855000, "3009857"};
-    user[1] = Bank{"Meet Radadiya", 300548556354, "SBI60459CO", 2500900, "3554848"};
-    user[2] = Bank{"Sumit Pavasiya", 308755647854, "SBI60459CO", 95000, "4584940"};
-    user[3] = Bank{"Mansi Limbasiya", 30089715564974, "SBI60459CO", 15800, "5484353"};
-    user[4] = Bank{"Divy Kachhadiya", 309795564763, "SBI60459CO", 89580, "3458584"};
-    user[5] = Bank{"Harshil Kanani", 309795526435, "SBI60459CO", 656580, "3485833"};
-    user[6] = Bank{"Meet Kanani", 309795586471, "SBI60459CO", 465280, "7777777"};
-    user[7] = Bank{"Vraj Patoliya", 309795585412, "SBI60459CO", 6580, "4584845"};
-    user[8] = Bank{"Raj Kheni", 309795415426, "SBI60459CO", 481280, "3485345"};
-    user[9] = Bank{"Ayush Gajera", 309795585252, "SBI60459CO", 481280, "4383438"};
-    user[10] = Bank{"Meet Kachhadiya", 301556477845, "SBI60459CO", 855000, "3049857"};
-    user[11] = Bank{"Meet Kheni", 302548556354, "SBI60459CO", 2500900, "3564848"};
-    user[12] = Bank{"Smit Vadrodiya", 303755647854, "SBI60459CO", 95000, "4784940"};
-    user[13] = Bank{"Denish Gajera", 30049715564974, "SBI60459CO", 15800, "1084353"};
-    user[14] = Bank{"Tanish Gajera", 305795564763, "SBI60459CO", 89580, "3459584"};
-    user[15] = Bank{"Dishant Kathiriya", 306795526435, "SBI60459CO", 656580, "1185833"};
+    user[0] = Bank{"Vraj Kachhadiya", 300556477845, "SBI60459CO", 855000, "1234"}; //1
+    user[1] = Bank{"Meet Radadiya", 300548556354, "SBI60459CO", 2500900, "4567"}; //2
+    user[2] = Bank{"Sumit Pavasiya", 308755647854, "SBI60459CO", 95000, "8910"}; //3
+    user[3] = Bank{"Mansi Limbasiya", 30089715564974, "SBI60459CO", 15800, "1112"}; //4
+    user[4] = Bank{"Divy Kachhadiya", 309795564763, "SBI60459CO", 89580, "1314"}; //5 
+    user[5] = Bank{"Harshil Kanani", 309795526435, "SBI60459CO", 656580, "1516"};  //6
+    user[6] = Bank{"Meet Kanani", 309795586471, "SBI60459CO", 465280, "1718"}; //7
+    user[7] = Bank{"Vraj Patoliya", 309795585412, "SBI60459CO", 6580, "1920"}; //8
+    user[8] = Bank{"Raj Kheni", 309795415426, "SBI60459CO", 481280, "2122"}; //9 
+    user[9] = Bank{"Ayush Gajera", 309795585252, "SBI60459CO", 481280, "2324"}; //10
+    user[10] = Bank{"Meet Kachhadiya", 301556477845, "SBI60459CO", 855000, "2526"}; //11
+    user[11] = Bank{"Meet Kheni", 302548556354, "SBI60459CO", 2500900, "2728"};  //12
+    user[12] = Bank{"Smit Vadrodiya", 303755647854, "SBI60459CO", 95000, "2930"}; //13
+    user[13] = Bank{"Denish Gajera", 30049715564974, "SBI60459CO", 15800, "3132"}; //14
+    user[14] = Bank{"Tanish Gajera", 305795564763, "SBI60459CO", 89580, "3334"}; //15
+    user[15] = Bank{"Dishant Kathiriya", 306795526435, "SBI60459CO", 656580, "3536"}; //16
 
     int choice;
 
@@ -110,7 +110,7 @@ int main()
         cout << "Press 1 To Check Balance." << endl;
         cout << "Press 2 For Deposit." << endl;
         cout << "Press 3 For Withdrow." << endl;
-        cout << "Press 4 To Display Details.";
+        cout << "Press 4 To Display Details." << endl;
         cout << "Press 0 For Exit." << endl;
         cout << endl;
         cout << endl;
@@ -133,7 +133,7 @@ int main()
         cout << "Enter User Number = ";
         cin >> n;
 
-        if (n < 0 || n > 15)
+        if (n < 0 || n > 16)
         {
             cout << endl
                  << "Invalid User Number !" << endl
