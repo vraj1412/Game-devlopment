c9fe65220421eb27890afc7e2c647135dc76d730
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-09-13 17:19:20 +0530

    13-9-24

--- CPP/BANK_PRO.cpp
+++ CPP/BANK_PRO.cpp
@@ -1,17 +1,18 @@
+
 #include <iostream>
 using namespace std;
 class Bank
 {
 protected:
     string name;
-    double ac_no;
+    long long ac_no;
     string pass;
     string ifsc;
-    double ac_bal;
+    long long ac_bal;
 
 public:
     Bank() {};
-    Bank(string name1, long long int Ac_number, string code, double balance, string Password1)
+    Bank(string name1, long long int Ac_number, string code, long long balance, string Password1)
     {
         name = name1;
         ac_no = Ac_number;
@@ -25,7 +26,7 @@ public:
         if (temp != pass)
         {
             cout << endl;
-            cout << "Invalid Input";
+            cout << "Invalid Input"<<endl;
             return 0;
         }
         return 1;
@@ -38,58 +39,66 @@ public:
         cout<<endl;
     }
 
-    void deposit(double money)
+    void deposit(long long money)
     {
         if (money <= 0)
         {   cout<<endl;
             cout<<endl;
-            cout << "Invalid Input";
+            cout << "Invalid Input"<<endl;
         }
         else
         {
             ac_bal += money;
             cout<<endl;
             cout<<endl;
-            cout << "Your Money " << money << " is Succesfully Creadited In Account"<< endl;
+            cout << "Your Money " << money << " is Succesfully Creadited In Account no "<<ac_no<< endl;
 
         }
     }
 
-    void withdraw(double money)
+    void withdraw(long long money)
     {
         if (money >= ac_bal)
         {
-            cout << "Invalid Input";
+            cout << "Invalid Input"<<endl;
         }
         else
         {
             ac_bal -= money;
             cout<<endl;
             cout<<endl;
-            cout << "Your Money " << money << " is Succesfully Debited In Account"<< endl;
+            cout << "Your Money " << money << " is Succesfully Debited In Account no "<<ac_no<< endl;
         }
+    }
+    void display(){
+        cout<<endl;
+        cout<<endl;
+        cout<<"Name :- "<< name<<endl;
+        cout<<"Account No :- "<<ac_no<<endl;
+        cout<<"IFSC code :- "<<ifsc<<endl;
+        cout<<"Balance :- "<<ac_bal<<endl;
     }
 };
 int main()
 {
     Bank user[15];
 
-    user[0] = Bank{"Vraj Kachhadiya", 300556477845, "SBI60459CO", 855000.45, "3009857"};
-    user[1] = Bank{"Meet Radadiya", 300548556354, "SBI60459CO", 2500900.93, "3554848"};
-    user[2] = Bank{"Sumit Pavasiya", 308755647854, "SBI60459CO", 95000.46, "4584940"};
-    user[3] = Bank{"Mansi Limbasiya", 30089715564974, "SBI60459CO", 15800.45, "5484353"};
-    user[4] = Bank{"Divy Kachhadiya", 309795564763, "SBI60459CO", 89580.45, "3458584"};
-    user[5] = Bank{"Harshil Kanani", 309795526435, "SBI60459CO", 656580.45, "3485833"};
-    user[6] = Bank{"Meet Kanani", 309795586471, "SBI60459CO", 465280.45, "7777777"};
-    user[7] = Bank{"Vraj Patoliya", 309795585412, "SBI60459CO", 6580.45, "4584845"};
-    user[8] = Bank{"Raj Kheni", 309795415426, "SBI60459CO", 481280.45, "3485345"};
-    user[9] = Bank{"Ayush Gajera", 309795585252, "SBI60459CO", 481280.45, "4383438"};
-    user[10] = Bank{"Meet Kachhadiya", 301556477845, "SBI60459CO", 855000.45, "3049857"};
-    user[11] = Bank{"Meet Kheni", 302548556354, "SBI60459CO", 2500900.93, "3564848"};
-    user[12] = Bank{"Smit Vadrodiya", 303755647854, "SBI60459CO", 95000.46, "4784940"};
-    user[13] = Bank{"Denish Gajera", 30049715564974, "SBI60459CO", 15800.45, "1084353"};
-    user[14] = Bank{"Tanish Gajera", 305795564763, "SBI60459CO", 89580.45, "3459584"};
-    user[15] = Bank{"Dishant Kathiriya", 306795526435, "SBI60459CO", 656580.45, "1185833"};
+    user[0] = Bank{"Vraj Kachhadiya", 300556477845, "SBI60459CO", 855000, "1234"}; //1
+    user[1] = Bank{"Meet Radadiya", 300548556354, "SBI60459CO", 2500900, "4567"}; //2
+    user[2] = Bank{"Sumit Pavasiya", 308755647854, "SBI60459CO", 95000, "8910"}; //3
+    user[3] = Bank{"Mansi Limbasiya", 30089715564974, "SBI60459CO", 15800, "1112"}; //4
+    user[4] = Bank{"Divy Kachhadiya", 309795564763, "SBI60459CO", 89580, "1314"}; //5 
+    user[5] = Bank{"Harshil Kanani", 309795526435, "SBI60459CO", 656580, "1516"};  //6
+    user[6] = Bank{"Meet Kanani", 309795586471, "SBI60459CO", 465280, "1718"}; //7
+    user[7] = Bank{"Vraj Patoliya", 309795585412, "SBI60459CO", 6580, "1920"}; //8
+    user[8] = Bank{"Raj Kheni", 309795415426, "SBI60459CO", 481280, "2122"}; //9 
+    user[9] = Bank{"Ayush Gajera", 309795585252, "SBI60459CO", 481280, "2324"}; //10
+    user[10] = Bank{"Meet Kachhadiya", 301556477845, "SBI60459CO", 855000, "2526"}; //11
+    user[11] = Bank{"Meet Kheni", 302548556354, "SBI60459CO", 2500900, "2728"};  //12
+    user[12] = Bank{"Smit Vadrodiya", 303755647854, "SBI60459CO", 95000, "2930"}; //13
+    user[13] = Bank{"Denish Gajera", 30049715564974, "SBI60459CO", 15800, "3132"}; //14
+    user[14] = Bank{"Tanish Gajera", 305795564763, "SBI60459CO", 89580, "3334"}; //15
+    user[15] = Bank{"Dishant Kathiriya", 306795526435, "SBI60459CO", 656580, "3536"}; //16
 
     int choice;
 
@@ -101,18 +110,19 @@ int main()
         cout << "Press 1 To Check Balance." << endl;
         cout << "Press 2 For Deposit." << endl;
         cout << "Press 3 For Withdrow." << endl;
+        cout << "Press 4 To Display Details." << endl;
         cout << "Press 0 For Exit." << endl;
         cout << endl;
         cout << endl;
 
-        cout << "Enter Your Choice [0-3] = ";
+        cout << "Enter Your Choice [0-4] = ";
         cin >> choice;
 
         if (choice == 0)
         {
             goto end;
         }
-        else if (choice < 0 || choice > 3)
+        else if (choice < 0 || choice > 4)
         {
             cout << "Invalid Input!" << endl;
             goto end;
@@ -123,7 +133,7 @@ int main()
         cout << "Enter User Number = ";
         cin >> n;
 
-        if (n < 0 || n > 15)
+        if (n < 0 || n > 16)
         {
             cout << endl
                  << "Invalid User Number !" << endl
@@ -161,6 +171,10 @@ int main()
             cout << "Enter the amount : ";
             cin >> amt;
             user[n - 1].withdraw(amt);
+        }
+        else if (choice == 4)
+        {
+            user[n - 1].display();
         }
    
         else if (choice == 0)
--- CPP/HOTEL_MANAGEMENT.cpp
+++ CPP/HOTEL_MANAGEMENT.cpp
@@ -2,37 +2,38 @@
 using namespace std;
 class hotel
 {
+
 public:
-     int p[25];
-     string i[50];
+     int p[50], d, choice, a[25], b[25], z = 0, qty, total, gst, net_total, tot = 0;
+     string i[1000], name, number;
      hotel()
      {
-          i[0] = "Manchuriyan";
-          i[1] = "Veg-65";
-          i[2] = "Garlic-ball";
-          i[3] = "Chaines-Bhel";
-          i[4] = "Panir-Tika";
-          i[5] = "Dosa";
-          i[6] = "Maisur";
-          i[7] = "Gotalo";
-          i[8] = "Mendu-Vada";
-          i[9] = "Idli";
-          i[10] = "7-chesee";
-          i[11] = "Mix-Veg";
-          i[12] = "Margrita";
-          i[13] = "Paneer Tanduri";
-          i[14] = "Onion Pizza";
-          i[15] = "Kaju Carry";
-          i[16] = "Paneer Masala";
-          i[17] = "Paneer Tanduri";
-          i[18] = "Veg Paneer";
-          i[19] = "Aloo Gobhi";
-          i[20] = "Mazza";
-          i[21] = "Cock";
-          i[22] = "Sprit";
-          i[23] = "Butter-Milk";
-          i[24] = "Thumsup";
-          i[25] = "Sosyo";
+          i[0] = "Manchurian     ";
+          i[1] = "veg-65         ";
+          i[2] = "Garlic-ball    ";
+          i[3] = "Chaines-Bhel   ";
+          i[4] = "Panir-Tika     ";
+          i[5] = "Dosa           ";
+          i[6] = "Maisur         ";
+          i[7] = "Gotalo         ";
+          i[8] = "Mendu-Vada     ";
+          i[9] = "Idli           ";
+          i[10] = "7-chesee      ";
+          i[11] = "Mix-Veg       ";
+          i[12] = "Margrita      ";
+          i[13] = "Paneer-Tanduri";
+          i[14] = "Onion-Pizza   ";
+          i[15] = "Kaju-Carry    ";
+          i[16] = "Paneer-Masala ";
+          i[17] = "Paneer-Tanduri";
+          i[18] = "Veg-Paneer    ";
+          i[19] = "Aloo-Gobhi    ";
+          i[20] = "Mazza         ";
+          i[21] = "Cock          ";
+          i[22] = "Sprit         ";
+          i[23] = "Butter-Milk   ";
+          i[24] = "Thumsup       ";
+          i[25] = "Sosyo         ";
 
           p[0] = 150;
           p[1] = 120;
@@ -61,53 +62,181 @@ public:
           p[24] = 25;
           p[25] = 25;
      }
-     void menu()
+     void show_menu()
      {
-          cout << "----------------| Welcome To Maharaja Hotel|----------------" << endl;
-          cout << "----------------|         Chainies         |----------------" << endl;
-          cout << "          01    Manchurian           150" << endl;
-          cout << "          02    Veg-65               120" << endl;
-          cout << "          03    Garlic-ball          150" << endl;
-          cout << "          04    Chaines-Bhel         160" << endl;
-          cout << "          05    Panir-Tika           140" << endl;
+          cout << "-------------------------------------" << endl;
+          cout << "|           maharaja hotal          |" << endl;
+          cout << "-------------------------------------" << endl;
+          cout << "--------------|CHINESE|--------------" << endl;
+          cout << "no.           item              price" << endl;
+          cout << "01          Manchurian           150 " << endl;
+          cout << "02          Veg-65               120 " << endl;
+          cout << "03          Garlic-ball          160 " << endl;
+          cout << "04          Chaines-Bhel         140 " << endl;
+          cout << "05          Panir-Tika            50 " << endl;
 
-          cout << "----------------|       South Indian       |----------------" << endl;
+          cout << "-----------|SOUTH INDIAN |-----------" << endl;
+          cout << "no.           item              price" << endl;
+          cout << "06          Dosa                 150 " << endl;
+          cout << "07          Maisur               250 " << endl;
+          cout << "08          Gotalo               100 " << endl;
+          cout << "09          Mendu-Vada           100 " << endl;
+          cout << "10          Idli                 200 " << endl;
 
-          cout << "          06    Dosa                  50" << endl;
-          cout << "          07    Maisur               150" << endl;
-          cout << "          08    Gotalo               250" << endl;
-          cout << "          09    Mendu-Vada           100" << endl;
-          cout << "          10    Idli                 100" << endl;
+          cout << "--------------|PIZZA'S|--------------" << endl;
+          cout << "no.           item              price" << endl;
+          cout << "11          '7-chesee            250 " << endl;
+          cout << "12          Mix-Veg              230 " << endl;
+          cout << "13          Margrita             180 " << endl;
+          cout << "14          Paneer Tanduri       200 " << endl;
+          cout << "15          'Onion Pizza         200 " << endl;
 
-          cout << "----------------|          Pizza          |----------------" << endl;
+          cout << "--------------|PUNJABI|--------------" << endl;
+          cout << "no.           item              price" << endl;
+          cout << "16          Kaju Carry           220 " << endl;
+          cout << "17          Paneer Masala        200 " << endl;
+          cout << "18          Paneer Tanduri       250 " << endl;
+          cout << "19          Veg Paneer           150 " << endl;
+          cout << "20          Aloo Gobhi           150 " << endl;
 
-          cout << "          11    7-chesee             200" << endl;
-          cout << "          12    Mix-Veg              250" << endl;
-          cout << "          13    Margrita             240" << endl;
-          cout << "          14    Paneer Tanduri       250" << endl;
-          cout << "          15    Onion Pizza          230" << endl;
-
-          cout << "----------------|         Panjabi         |----------------" << endl;
-
-          cout << "          16    Kaju Carry           180" << endl;
-          cout << "          17    Paneer Masala        200" << endl;
-          cout << "          18    Paneer Tanduri       200" << endl;
-          cout << "          19    Veg Paneer           220" << endl;
-          cout << "          20    Aloo Gobhi           200" << endl;
-
-          cout << "----------------|         Drinks         |----------------" << endl;
-
-          cout << "          21    Mazza                 25" << endl;
-          cout << "          22    Cock                  25" << endl;
-          cout << "          23    Sprit                 25" << endl;
-          cout << "          24    Butter-Milk           25" << endl;
-          cout << "          25    Thumsup               25" << endl;
-          cout << "          26    Sosyo                 25" << endl;
+          cout << "--------------|DRINKS |--------------" << endl;
+          cout << "no.          item              price" << endl;
+          cout << "21          Mazza                 25 " << endl;
+          cout << "22          Cock                  25 " << endl;
+          cout << "23          Sprit                 25 " << endl;
+          cout << "24          Butter-Milk           25 " << endl;
+          cout << "25          Thumsup               25 " << endl;
      }
-     
+     void data()
+     {
+
+          cout << "================ Customer Details ================" << endl;
+
+          cout << "Enter Customer Name:";
+          cin >> name;
+
+     again:
+
+          cout << "Enter Customer Mobile Number[10 digit only]:";
+          cin >> number;
+
+          d = 0;
+          d = number.size();
+
+          if (d == 10)
+          {
+               cout << endl;
+          }
+          else
+          {
+               cout << "Invalid Number" << endl;
+               goto again;
+          }
+     }
+     void order()
+     {
+
+          cout << "Enter your choice : ";
+          cin >> choice;
+          if (choice == 0)
+          {
+               cout << "valid number";
+          }
+          else
+          {
+
+               a[z] = choice;
+               cout << "Enter quantity :";
+               cin >> qty;
+
+               b[z] = qty;
+               z++;
+               cout << endl;
+          }
+     }
+     void bill()
+     {
+          z--;
+          int n, f = 0, t = 0;
+          cout << "-----------------------------------------------------------------" << endl;
+          cout << "                         maharaja hotal                          " << endl;
+          cout << "-----------------------------------------------------------------" << endl;
+          cout << "   NAME   :" << name << endl;
+          cout << "   NUMBER :" << number << endl;
+          cout << "-----------------------------------------------------------------" << endl;
+          cout << "-----------------------------------------------------------------" << endl;
+          cout << " NO\t\tITEM\t\t      QTY\t\tPRICE\t\tTOTAL\t\t" << endl;
+          for (int j = 0; j <= z; j++)
+          {
+
+               n = a[j];
+
+               f = b[j];
+               n--;
+
+               total = p[n] * f;
+
+               cout  << j + 1 <<"           ";
+
+               cout <<"   "<< i[n] << "\t\t" << f << "\t\t" << p[n] << "\t\t" << total << endl;
+               t = f*p[n];
+
+          tot = tot + t;
+          gst = tot * 0.18;
+          net_total = tot + gst;
+          }
+          
+          cout << "-----------------------------------------------------------------" << endl;
+          cout << "|total bill:                                            " << tot << endl;
+          cout << "|gst:                                                   " << gst << endl;
+          cout << "-----------------------------------------------------------------" << endl;
+          cout << "|after gst add bill:                                    " << net_total << endl;
+          cout << "-----------------------------------------------------------------" << endl;
+     }
 };
 int main()
 {
-     
+     hotel s1;
+     s1.data();
+     int ac;
+
+     do
+     {
+          {
+               cout << "-----------------------------------------------------------------" << endl;
+               cout << "                          maharaja hotal                         " << endl;
+               cout << "-----------------------------------------------------------------" << endl;
+               cout << "                        Choose Your Option:                      " << endl;
+               cout << "                        enter 1 for  menu                        " << endl;
+               cout << "                        enter 2 for  food order                  " << endl;
+               cout << "                        enter 3 for  bill                        " << endl;
+               cout << "                        enter 0 for  Exit                        " << endl;
+               cout << "-----------------------------------------------------------------" << endl;
+
+               cout << "   Enter Your choice   : ";
+               cin >> ac;
+
+               switch (ac)
+               {
+               case 1:
+                    s1.show_menu();
+                    break;
+               case 2:
+                    s1.order();
+                    break;
+               case 3:
+                    s1.bill();
+                    break;
+               case 0:
+                    cout << "--------------------------------" << endl;
+                    cout << "       Thank you for visit      " << endl;
+                    cout << "--------------------------------" << endl;
+                    return 0;
+               default:
+                    cout << "|   enter valid number   |" << endl;
+               }
+          }
+     } while (ac != 0);
+
      return 0;
 }
